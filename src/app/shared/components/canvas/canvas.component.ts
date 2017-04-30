62dece4b5321c5bd0cb1d973cb51bf8ceebfc32d
Matthew Whitt <mwhitt.w@gmail.com>
2017-04-29 19:03:25 -0700

    fix(zoom + pan): fix placement issues
    still a little wonky with panning, clipart doesn't move at exactly the
    same speed as the grid. I'll solve this by panning over an existing
    offscreen canvas.

--- src/app/shared/components/canvas/canvas.component.ts
+++ src/app/shared/components/canvas/canvas.component.ts
@@ -16,22 +16,31 @@ export class BatMapCanvasComponent {
   private _canvas: ElementRef;
 
   private resizeTimeout: any;
-  private baseCellSize: number = 5;
+  private baseCellSize: number = 10;
   private rootY: number = 0;
   private rootX: number = 0;
   private mouseClickState: string = "up";
+  private zoomLevel: number = 10;
+  private mapOffsetX: number = 0;
+  private mapOffsetY: number = 0;
 
-  private zoomLevel: number = 15;
+  private gridX: number = 0;
+  private gridY: number = 0;
+  private clipX: number = 0;
+  private clipY: number = 0;
+
+  private store: any[] = [];
 
   public ngAfterViewInit() {
-    this.drawGrid();
+    this.ctx.imageSmoothingEnabled = false;
+    this.render();
   }
 
   @HostListener('window:resize', ['$event'])
   public onResize(event) {
     clearTimeout(this.resizeTimeout);
     this.resizeTimeout = setTimeout(() => {
-      this.drawGrid();
+      this.render();
     }, 20);
   }
 
@@ -39,11 +48,31 @@ export class BatMapCanvasComponent {
     return this.zoomLevel * this.baseCellSize;
   }
 
-  public zoom(increment: number) {
-    this.zoomLevel += increment;
-    this.zoomLevel = this.zoomLevel || 1;
-    this.clearGrid();
-    this.drawGrid();
+  public onZoom(zoomType: string) {
+    let oldZoom: number = this.zoomLevel;
+
+    if (zoomType === "in") {
+      this.zoomLevel += 2;
+    } else if (zoomType === "out" && this.zoomLevel !== 2) {
+      this.zoomLevel += -2;
+    } else {
+      return;
+    }
+
+    let zoomPercentage = this.zoomLevel / oldZoom;
+    let onZoom = true;
+    this.store.forEach((item) => {
+      item.realX += this.mapOffsetX - (this.mapOffsetX % 100);
+      item.realY += this.mapOffsetY - (this.mapOffsetY % 100);
+      item.realX *= zoomPercentage;
+      item.realY *= zoomPercentage;
+      item.zoomLevel = this.zoomLevel;
+    });
+    this.rootX = 0;
+    this.rootY = 0;
+    this.mapOffsetX = 0;
+    this.mapOffsetY = 0;
+    this.render();
   }
 
   public get viewPortWidth() {
@@ -78,25 +107,53 @@ export class BatMapCanvasComponent {
     return this.canvas.getBoundingClientRect().width;
   }
 
-  public drawGrid() {
+  public offsetGrid(e: any): void {
+    if (this.rootX > this.cellSize) {
+      this.rootX = 0;
+    } else if (this.rootX < 0) {
+      this.rootX = this.cellSize;
+    } else {
+      this.rootX += e.movementX;
+    }
+
+    if (this.rootY > this.cellSize) {
+      this.rootY = 0;
+    } else if (this.rootY < 0) {
+      this.rootY = this.cellSize;
+    } else {
+      this.rootY += e.movementY;
+    }
+    console.log("Roots!", this.rootX, this.rootY)
+  }
+
+  public drawBackground() {
     this.clearGrid();
     this.setBackgroundColor();
+  }
+
+  public panLeft() {
+
+  }
+
+  public drawGrid() {
+    let offsetX = this.mapOffsetX % 100;
+    let offsetY = this.mapOffsetY % 100;
     for (
       let x = 0 * this.zoomLevel;
       x < this.cw;
       x += this.cellSize) {
-      this.ctx.moveTo(this.rootX + .5 + x, 0);
+      this.ctx.moveTo(offsetX + .5 + x, 0);
       this.ctx.lineWidth = 1;
-      this.ctx.lineTo(this.rootX + .5 + x, this.ch);
+      this.ctx.lineTo(offsetX + .5 + x, this.ch);
     }
 
     for (
       let y = 0 * this.zoomLevel;
       y < this.ch;
       y += this.cellSize) {
-      this.ctx.moveTo(0, this.rootY + .5 + y);
+      this.ctx.moveTo(0, offsetY + .5 + y);
       this.ctx.lineWidth = 1;
-      this.ctx.lineTo(this.cw, this.rootY + .5 + y);
+      this.ctx.lineTo(this.cw, offsetY + .5 + y);
     }
 
     this.ctx.strokeStyle = "#b7b7b7";
@@ -115,39 +172,69 @@ export class BatMapCanvasComponent {
   public onMouseDown(e) {
     this.mouseClickState = "down";
     if (this.isImageSelected) {
-      let imageHeight = this.cursorImage.getBoundingClientRect().height;
-      let imageWidth = this.cursorImage.getBoundingClientRect().width;
-      this.ctx.imageSmoothingEnabled = false;
-      this.ctx.drawImage(this.cursorImage, e.clientX - imageWidth / 2, e.clientY - imageHeight / 2);
+      let realX = e.clientX + (this.mapOffsetX * -1);
+      let realY = e.clientY + (this.mapOffsetY * -1);
+      if (realX % 2 !== 0) {
+        realX += 1;
+      }
+      if (realY % 2 !== 0) {
+        realY += 1;
+      }
+      this.store.push({
+        src: this.cursorImage.src,
+        realHeight: this.cursorImage.height / this.zoomLevel,
+        realWidth: this.cursorImage.width / this.zoomLevel,
+        realX: realX,
+        realY: realY,
+        realZoom: this.zoomLevel,
+      });
+      this.drawAllClipart();
+      this.drawGrid();
     }
+  }
+
+  public left() {
+    this.paneX(-5);
+  }
+
+  public right() {
+    this.paneX(5);
+  }
+
+  public up() {
+    this.paneY(-5);
+  }
+
+  public down() {
+    this.paneY(5);
+  }
+
+  public paneX(increment: number) {
+    this.mapOffsetX += increment;
+    this.rootX += increment;
+    this.render();
+  }
+
+  public paneY(increment: number) {
+    this.mapOffsetY += increment;
+    this.rootY += increment;
+    this.render();
   }
 
   public onMouseUp(e) {
     this.mouseClickState = "up";
+    this.drawGrid();
   }
 
+  public eventNumber: number = 0;
   public onMouseMove(e) {
     if (this.mouseClickState === "up") {
       return;
     } else if (this.mouseClickState === "down") {
-
-      if (this.rootX > this.cellSize) {
-        this.rootX = 0;
-      } else if (this.rootX < 0) {
-        this.rootX = this.cellSize;
-      } else {
-        this.rootX += e.movementX;
-      }
-
-      if (this.rootY > this.cellSize) {
-        this.rootY = 0;
-      } else if (this.rootY < 0) {
-        this.rootY = this.cellSize;
-      } else {
-        this.rootY += e.movementY;
-      }
-
-      this.drawGrid();
+      this.mapOffsetX += e.movementX;
+      this.mapOffsetY += e.movementY;
+      this.offsetGrid(e);
+      this.render();
     }
   }
 
@@ -175,10 +262,8 @@ export class BatMapCanvasComponent {
 
   public followCursor(e) {
     if (!!this._cursorImage) {
-      let imageHeight = this.cursorImage.getBoundingClientRect().height;
-      let imageWidth = this.cursorImage.getBoundingClientRect().width;
-      this.iconImageTop = e.clientY - imageHeight / 2;
-      this.iconImageLeft = e.clientX - imageWidth / 2;
+      this.iconImageTop = e.clientY;
+      this.iconImageLeft = e.clientX;
     }
   }
 
@@ -190,4 +275,37 @@ export class BatMapCanvasComponent {
     }
   }
 
+  public centeredCoord(coord: number, dimension: number) {
+    return coord - dimension / 2
+  }
+
+  public drawAllClipart(onZoom?: boolean): void {
+    for (let i = 0; i < this.store.length; i++) {
+      this.drawClipart(i);
+    };
+  }
+
+  public drawClipart(index: number) {
+    if (index >= this.store.length) {
+      return;
+    }
+    let item = this.store[index];
+    let img = new Image();
+    img.src = item.src;
+
+
+    this.ctx.drawImage(
+      img,
+      (item.realX + this.mapOffsetX),
+      (item.realY + this.mapOffsetY),
+      item.realHeight * this.zoomLevel,
+      item.realWidth * this.zoomLevel,
+    );
+  }
+
+  public render() {
+    this.drawBackground();
+    this.drawAllClipart();
+    this.drawGrid();
+  }
 }
